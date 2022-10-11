f83371860f467685192b73b00bc708ed8944678c
simon-dutka <SimonDutkaWork@gmail.com>
2022-10-11 17:46:53 +0200

    Added projects table

--- website/app.py
+++ website/app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
