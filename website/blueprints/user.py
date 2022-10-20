5829feea288b5be8d99922910f567bc82654cf15
Simon Dutka <SimonDutkaWork@gmail.com>
2022-10-20 18:04:49 +0200

    Added verification if user is logged on /user subpage

--- website/blueprints/user.py
+++ website/blueprints/user.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from models import  User
+from utils.auth import login_required
 
 user = Blueprint("user", __name__)
 
 @user.route("/", methods=["GET"])
-def index():
+@login_required(User)
+def index(user):
     return render_template("user/user.html")
