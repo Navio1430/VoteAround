c1c5e727ad4327b07eb55be299b13061f89e8aa1
Simon Dutka <SimonDutkaWork@gmail.com>
2022-10-22 18:56:31 +0200

    Changed styles for 'signup' page
    -Deleted events.html

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
+# Name
 
+Celem naszej apliakcji jest umożliwienie obywatelom na terenie całego kraju, tworzenia projektów które wpłyną na jakość życia lokalnego społeczeństwa. 
+
+Każdy obywatel po zarejestrowaniu ma możliwość głosowania (pozytywnie bądź negatywnie) na powstałe projekty, jak i tworzenia własnych.
+
+Podczas tworzenia projektu użytkownik podaje podstawowe dane na jego temat:
+
+- **Tytuł projektu** 
+- **Opis projektu**
+- **Obszar którego dotyczy projekt:**
+    - Jedynie użytkownicy znajdujący się w określonym obszarze mogą oddać głos na projekt
+
+Dane pozyskane dzięki naszej aplikajci pozwalają między innymi na oszacowanie poparcia danego projektu, co znacznie usprawni proces wdrożenia go w życie.
+
+Projekt wykonany przeznaczony na konkurs hackheroes 2022, wykonany pomiędzy 8 a 23 października.
+
+## Jak uruchomić projekt lokalnie
+**Uwaga aby poprawnie uruchmoić projekt, należy zainstalować [python](https://www.python.org/downloads/)**
+
+Skopiowanie projektu
+```bash
+  git clone https://link-to-project
+```
+
+Otwarcie folderu projektu
+
+Otwarcie terminala
+
+Instalacja zależności
+
+```bash
+  pip install -r requirements
+```
+
+Przejście do ścieżki
+
+```bash
+  cd /website
+```
+
+Uruchomienie projektu lokalnie
+
+```bash
+  python app.py
+```
+
+Otwarcie local host w przeglądarce, port 5000
+
+```bash
+  127.0.0.1:5000
+```
+## Twórcy
+
+- [@Navio1430](https://github.com/Navio1430)
+- [@simon-dutka](https://github.com/simon-dutka)
+- [@KotreQ](https://github.com/KotreQ)
--- website/static/dist/InfiniteScroll.js
+++ website/static/dist/InfiniteScroll.js
@@ -1,5 +1,5 @@
 const infiniteScroll = () => {
-    const tableRowContainer = document.getElementById("table-row-container");
+    const tableRowContainer = document.getElementById('table-row-container');
     let fetchedProjects = [];
     async function fetchData(index, limit) {
         const response = await fetch(`/api/projects/popular?index=${index}&limit=${limit}`);
@@ -18,31 +18,31 @@ const infiniteScroll = () => {
         await fetchData(indexNum, 4);
         indexNum += fetchedProjects.length;
         fetchedProjects.forEach((project) => {
-            let projectHtmlElement = document.createElement("div");
-            projectHtmlElement.classList.add("table__row");
+            let projectHtmlElement = document.createElement('div');
+            projectHtmlElement.classList.add('table__row');
             let pItems = [];
             for (let i = 0; i <= 3; i++) {
-                let pItem = document.createElement("p");
-                pItem.classList.add("table__row-item");
+                let pItem = document.createElement('p');
+                pItem.classList.add('table__row-item');
                 pItems.push(pItem);
             }
-            pItems[0].classList.add("table__row-item");
-            let labelA = document.createElement("a");
+            pItems[0].classList.add('table__row-item');
+            let labelA = document.createElement('a');
             labelA.innerHTML = project.label;
             labelA.href = `${project.uuid}`;
             pItems[0].appendChild(labelA);
             pItems[1].innerHTML = project.description;
             pItems[2].innerHTML = project.positive_votes.toString();
             pItems[3].innerHTML = project.negative_votes.toString();
-            let btnPositive = document.createElement("button");
-            btnPositive.classList.add("table__row-btn", "table__row-btn--positive");
-            let btnPositiveA = document.createElement("a");
-            btnPositiveA.innerHTML = "Za";
+            let btnPositive = document.createElement('button');
+            btnPositive.classList.add('table__row-btn', 'table__row-btn--positive');
+            let btnPositiveA = document.createElement('a');
+            btnPositiveA.innerHTML = 'Za';
             btnPositive.appendChild(btnPositiveA);
-            let btnNegative = document.createElement("button");
-            btnNegative.classList.add("table__row-btn", "table__row-btn--negative");
-            let btnNegativeA = document.createElement("a");
-            btnNegativeA.innerHTML = "Przeciw";
+            let btnNegative = document.createElement('button');
+            btnNegative.classList.add('table__row-btn', 'table__row-btn--negative');
+            let btnNegativeA = document.createElement('a');
+            btnNegativeA.innerHTML = 'Przeciw';
             btnNegative.appendChild(btnNegativeA);
             let projectItems = [
                 pItems[0],
@@ -61,7 +61,7 @@ const infiniteScroll = () => {
     }
     createTableItem();
     fetchedCount = fetchedProjects.length;
-    tableRowContainer.addEventListener("scroll", () => {
+    tableRowContainer.addEventListener('scroll', () => {
         let scrollTop = tableRowContainer.scrollTop;
         let scrollHeight = tableRowContainer.scrollHeight - tableRowContainer.clientHeight;
         let progress = scrollTop / scrollHeight;
