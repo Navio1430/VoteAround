bcd74c4d483128aae96698f7493540fd9a33ee2e
Simon Dutka <SimonDutkaWork@gmail.com>
2022-10-22 19:49:34 +0200

    Added account deletion

--- website/static/dist/DeleteAccount.js
+++ website/static/dist/DeleteAccount.js
@@ -9,7 +9,9 @@ const deleteAccount = () => {
         title.innerHTML = 'Usuwanie konta';
         let password = document.createElement('input');
         password.classList.add('user__delete-password');
+        password.type = 'password';
         password.placeholder = 'Podaj hasło';
+        let alertContainer = document.createElement('div');
         let checkbox = document.createElement('input');
         checkbox.classList.add('user__delete-checkbox');
         checkbox.type = 'checkbox';
@@ -20,19 +22,43 @@ const deleteAccount = () => {
         checkboxContainer.classList.add('user__delete-checkbox-container');
         checkboxContainer.appendChild(checkbox);
         checkboxContainer.appendChild(checkboxP);
+        alertContainer.appendChild(checkboxContainer);
         let btnP = document.createElement('p');
         btnP.innerHTML = 'Usuń';
         let btn = document.createElement('button');
         btn.classList.add('table__row-btn', 'table__row-btn--negative', 'user__delete-btn');
-        btn.type = 'submit';
         btn.appendChild(btnP);
-        let alertItems = [title, password, checkboxContainer, btn];
+        let alertItems = [title, password, alertContainer, btn];
         alertItems.forEach((element) => {
             alert.appendChild(element);
         });
         userContainer.children.length === 1
             ? userContainer.appendChild(alert)
             : userContainer.removeChild(userContainer.lastChild);
+        btn.addEventListener('click', () => {
+            if (checkbox.checked === true) {
+                fetch('/api/user/delete', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ password: password.value }),
+                })
+                    .then((response) => response.json())
+                    .then((data) => {
+                    if (data.success) {
+                        location.reload();
+                    }
+                    else {
+                        if (alertContainer.children.length <= 1) {
+                            let passwordAlert = document.createElement('p');
+                            passwordAlert.innerHTML = 'Błędne hasło';
+                            alertContainer.appendChild(passwordAlert);
+                        }
+                    }
+                });
+            }
+        });
     });
 };
 export { deleteAccount };
--- website/static/src/DeleteAccount.ts
+++ website/static/src/DeleteAccount.ts
@@ -12,7 +12,10 @@ const deleteAccount = () => {
 
         let password = document.createElement('input');
         password.classList.add('user__delete-password');
+        password.type = 'password';
         password.placeholder = 'Podaj hasło';
+
+        let alertContainer = document.createElement('div');
 
         let checkbox = document.createElement('input');
         checkbox.classList.add('user__delete-checkbox');
@@ -28,6 +31,8 @@ const deleteAccount = () => {
         checkboxContainer.appendChild(checkbox);
         checkboxContainer.appendChild(checkboxP);
 
+        alertContainer.appendChild(checkboxContainer);
+
         let btnP = document.createElement('p');
         btnP.innerHTML = 'Usuń';
 
@@ -37,11 +42,10 @@ const deleteAccount = () => {
             'table__row-btn--negative',
             'user__delete-btn'
         );
-        btn.type = 'submit';
 
         btn.appendChild(btnP);
 
-        let alertItems = [title, password, checkboxContainer, btn];
+        let alertItems = [title, password, alertContainer, btn];
         alertItems.forEach((element) => {
             alert.appendChild(element);
         });
@@ -49,6 +53,30 @@ const deleteAccount = () => {
         userContainer.children.length === 1
             ? userContainer.appendChild(alert)
             : userContainer.removeChild(userContainer.lastChild);
+
+        btn.addEventListener('click', () => {
+            if (checkbox.checked === true) {
+                fetch('/api/user/delete', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ password: password.value }),
+                })
+                    .then((response) => response.json())
+                    .then((data) => {
+                        if (data.success) {
+                            location.reload();
+                        } else {
+                            if (alertContainer.children.length <= 1) {
+                                let passwordAlert = document.createElement('p');
+                                passwordAlert.innerHTML = 'Błędne hasło';
+                                alertContainer.appendChild(passwordAlert);
+                            }
+                        }
+                    });
+            }
+        });
     });
 };
 
