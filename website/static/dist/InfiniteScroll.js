03dc18d8c27d168bedff5461c8acaadf1de886ee
Navio1430 <73886336+Navio1430@users.noreply.github.com>
2022-10-23 03:07:36 +0200

    Refactored InfiniteScroll script
    - Removed InfiniteScroll script initialization from app.js
    - Removed unused app.js file

--- website/static/dist/InfiniteScroll.js
+++ website/static/dist/InfiniteScroll.js
@@ -1,76 +1,68 @@
-const infiniteScroll = () => {
-    const tableRowContainer = document.getElementById('table-row-container');
-    let fetchedProjects = [];
-    async function fetchData(index, limit) {
-        const response = await fetch(`/api/projects/newest?index=${index}&limit=${limit}`);
-        const data = response.json();
-        await data.then((data) => {
-            data.forEach((dataProject) => {
-                fetchedProjects.push(dataProject);
-            });
-        });
-    }
-    let indexNum = 0;
-    let fetchedCount = 0;
-    let fetchedCountPrev = -1;
-    async function createTableItem() {
-        fetchedProjects = [];
-        await fetchData(indexNum, 4);
-        indexNum += fetchedProjects.length;
-        fetchedProjects.forEach((project) => {
-            let projectHtmlElement = document.createElement('div');
-            projectHtmlElement.classList.add('table__row');
-            let pItems = [];
-            for (let i = 0; i <= 3; i++) {
-                let pItem = document.createElement('p');
-                pItem.classList.add('table__row-item');
-                pItems.push(pItem);
-            }
-            pItems[0].classList.add('table__row-item');
-            let labelA = document.createElement('a');
-            labelA.innerHTML = project.label;
-            labelA.href = `${project.uuid}`;
-            pItems[0].appendChild(labelA);
-            pItems[1].innerHTML = project.description;
-            pItems[2].innerHTML = project.positive_votes.toString();
-            pItems[3].innerHTML = project.negative_votes.toString();
-            let btnPositive = document.createElement('button');
-            btnPositive.classList.add('table__row-btn', 'table__row-btn--positive');
-            let btnPositiveA = document.createElement('a');
-            btnPositiveA.innerHTML = 'Za';
-            btnPositive.appendChild(btnPositiveA);
-            let btnNegative = document.createElement('button');
-            btnNegative.classList.add('table__row-btn', 'table__row-btn--negative');
-            let btnNegativeA = document.createElement('a');
-            btnNegativeA.innerHTML = 'Przeciw';
-            btnNegative.appendChild(btnNegativeA);
-            let projectItems = [
-                pItems[0],
-                pItems[1],
-                pItems[2],
-                pItems[3],
-                btnPositive,
-                btnNegative,
-            ];
-            projectItems.forEach((item) => {
-                projectHtmlElement.appendChild(item);
-            });
-            //* Adding item to table row
-            tableRowContainer.appendChild(projectHtmlElement);
-        });
-    }
-    createTableItem();
-    fetchedCount = fetchedProjects.length;
-    tableRowContainer.addEventListener('scroll', () => {
-        let scrollTop = tableRowContainer.scrollTop;
-        let scrollHeight = tableRowContainer.scrollHeight - tableRowContainer.clientHeight;
-        let progress = scrollTop / scrollHeight;
-        if (progress >= 0.9 && fetchedCount !== fetchedCountPrev) {
-            fetchedCountPrev = fetchedCount;
-            createTableItem().then(() => {
-                fetchedCount += fetchedProjects.length;
-            });
-        }
+const rowsContainer = document.getElementById('table-row-container');
+const limit = 5;
+let index = 0;
+loadNext();
+async function getNext(index, limit) {
+    let response = await fetch(`/api/projects/newest?index=${index}&limit=${limit}`);
+    return await response.json().then((data) => {
+        return data;
     });
-};
-export { infiniteScroll };
+}
+async function loadNext() {
+    let projects = await getNext(index, limit);
+    projects.forEach(element => {
+        createRow(element.uuid, element.label, element.description, element.positive_votes, element.negative_votes);
+    });
+    if (projects.length === 0)
+        return;
+    index += limit;
+}
+function createRow(uuid, label, description, positive_votes, negative_votes) {
+    let row = document.createElement('div');
+    row.classList.add('table__row');
+    let items = [];
+    for (let i = 0; i <= 3; i++) {
+        let item = document.createElement('p');
+        item.classList.add('table__row-item');
+        items.push(item);
+    }
+    items[0].classList.add('table__row-item');
+    let label_item = document.createElement('a');
+    label_item.innerHTML = label;
+    label_item.href = `${uuid}`;
+    items[0].appendChild(label_item);
+    items[1].innerHTML = description;
+    items[2].innerHTML = positive_votes.toString();
+    items[3].innerHTML = negative_votes.toString();
+    let btnPositive = document.createElement('button');
+    btnPositive.classList.add('table__row-btn', 'table__row-btn--positive');
+    let btnPositiveInner = document.createElement('a');
+    btnPositiveInner.innerHTML = 'Za';
+    btnPositive.appendChild(btnPositiveInner);
+    let btnNegative = document.createElement('button');
+    btnNegative.classList.add('table__row-btn', 'table__row-btn--negative');
+    let btnNegativeInner = document.createElement('a');
+    btnNegativeInner.innerHTML = 'Przeciw';
+    btnNegative.appendChild(btnNegativeInner);
+    let rowItems = [
+        items[0],
+        items[1],
+        items[2],
+        items[3],
+        btnPositive,
+        btnNegative,
+    ];
+    rowItems.forEach((item) => {
+        row.appendChild(item);
+    });
+    rowsContainer.appendChild(row);
+}
+rowsContainer.addEventListener('scroll', () => {
+    let scrollTop = rowsContainer.scrollTop;
+    let scrollHeight = rowsContainer.scrollHeight - rowsContainer.clientHeight;
+    let progress = scrollTop / scrollHeight;
+    if (progress === 1) {
+        loadNext();
+    }
+});
+export {};
--- website/static/dist/app.js
+++
@@ -1,4 +0,0 @@
-import { infiniteScroll } from './InfiniteScroll.js';
-if (document.getElementById('table-row-container') !== null) {
-    infiniteScroll();
-}
