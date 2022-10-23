0aacdc8a38a14e6eccd9ad1bf9b06d995a93aff9
Simon Dutka <SimonDutkaWork@gmail.com>
2022-10-23 13:38:32 +0200

    Added popular cards showing

---
+++ website/static/dist/PopularCard.js
@@ -0,0 +1,13 @@
+// <div class="card__container">
+//             <a href="#">
+//                 <div class="card">
+//                     <img
+//                         src="{{ url_for('static', filename='assets/city.svg') }}"
+//                         alt=""
+//                         class="card__icon"
+//                     />
+//                     <p class="card__title">Lorem ipsum</p>
+//                 </div>
+//             </a>
+//         </div>
+export {};
